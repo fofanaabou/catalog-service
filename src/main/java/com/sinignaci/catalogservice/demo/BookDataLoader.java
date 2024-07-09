fd66f522739aa3ac12c66b63065a728569e55c9a
aboufofana <aboufofana76@gmail.com>
2024-07-09 22:08:15 +0200

    feed memory database

---
+++ src/main/java/com/sinignaci/catalogservice/demo/BookDataLoader.java
@@ -0,0 +1,28 @@
+package com.sinignaci.catalogservice.demo;
+
+import com.sinignaci.catalogservice.domain.Book;
+import com.sinignaci.catalogservice.persistence.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("test-data")
+@RequiredArgsConstructor
+public class BookDataLoader implements CommandLineRunner  {
+
+    private final BookRepository bookRepository;
+    @Override
+    public void run(String... args) {
+        loadBookTestData();
+    }
+    private void loadBookTestData() {
+        var book1 = new Book("1234567891", "Northern Lights",
+                "Lyra Silverstar", 9.90);
+        var book2 = new Book("1234567892", "Polar Journey",
+                "Iorek Polarson", 12.90);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
